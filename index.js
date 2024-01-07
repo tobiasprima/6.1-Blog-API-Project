c4db3ff84f6e347e25054e4639f84a3289cebb06
Tobias Prima <primatjahja@gmail.com>
2024-01-07 18:07:51 +0800

    feat: patch a post

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.get("/posts", (req,res)=> {
 
 //CHALLENGE 2: GET a specific post by id
 app.get("/posts/:id", (req,res)=> {
-  const post = post.find((p) => p.id === parseInt(req.params.id));
+  const post = posts.find((p) => p.id === parseInt(req.params.id));
   if (!post) return res.status(404).json({message: "Post not found"});
   res.json(post);
 })
@@ -68,6 +68,14 @@ app.post("/posts", (req,res)=> {
 })
 
 //CHALLENGE 4: PATCH a post when you just want to update one parameter
+app.patch("/posts/:id", (req,res) => {
+  const post = posts.find((p)=> p.id === parseInt(req.params.id));
+  if(!post) return res.status(404).json({message: "Post not found"});
+  
+  if(req.body.title) post.title = req.body.title;
+  if(req.body.content) post.content = req.body.content;
+  if(req.body.author) post.author = req.body.author;
+})
 
 //CHALLENGE 5: DELETE a specific post by providing the post id.
 
